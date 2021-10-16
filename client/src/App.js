f9a70a919b00658bd96a49981b50c3f5c2d2b521
Jacob Pfeifer <jacob@pfeifer.dev>
2021-10-16 18:04:51 -0400

    added routes for getting cover art and metadata

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import axios from 'axios';
+import get from 'lodash/get';
 
 function App() {
+  const [metadata, setMetadata] = useState({});
+  useEffect(async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/book/metadata?filename=book.m4a');
+      setMetadata(response.data);
+    } catch(e) {
+      console.error(e);
+    }
+  });
+  document.title = get(metadata, "format.tags.title", "Audiobook");
   return (
     <div className="App">
+      <img src="http://localhost:3001/book/cover?filename=book.m4a" />
       <AudioPlayer
           autoPlay
           showSkipControls
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded())
 const port = 3001
@@ -55,10 +60,7 @@ async function main() {
   app.get('/book', function(req, res) {
     // heavily based on https://betterprogramming.pub/video-stream-with-node-js-and-html5-320b3191a6b6
     const book = req.query.filename;
-    const bookPath = path.join('assets', book)
-    if(!bookPath.startsWith('assets' + path.sep)){
-      throw new Error('Book path left assets directory');
-    }
+    const bookPath = getBookPath(book);
     const stat = fs.statSync(bookPath)
     const fileSize = stat.size
     const range = req.headers.range
@@ -91,10 +93,42 @@ async function main() {
   return 'done.';
 }
 
+app.get('/book/metadata', async (req, res) => {
+  const book = req.query.filename;
+  const bookPath = getBookPath(book);
+  const metadata = await getMetadata(bookPath);
+  res.send(metadata);
+});
+
+app.get('/book/cover', async (req, res) => {
+  const book = req.query.filename;
+  const bookPath = getBookPath(book);
+  const cover = await getCoverArt(bookPath);
+  res.send(cover);
+});
+
 app.use('/', express.static('../client/build'));
+
+function getBookPath(filename) {
+  const bookPath = path.join('assets', filename)
+  if(!bookPath.startsWith('assets' + path.sep)){
+    throw new Error('Book path not in assets directory');
+  }
+  return bookPath;
+}
+
+async function getMetadata(path) {
+  const {stdout} = await exec(`ffprobe -i "${path}" -print_format json -show_chapters -show_format -loglevel error`)
+  const metadata = JSON.parse(stdout);
+  return metadata;
+}
+
+async function getCoverArt(path) {
+  const {stdout} = await exec(`ffmpeg -i "${path}" -an -f singlejpeg -hide_banner -loglevel error -`, { encoding: "binary" })
+  const file = Buffer.from(stdout, "binary")
+  return file;
+}
 
 main()
   .then(console.log)
   .catch(console.error)
-
-//ffprobe -i assets/book.m4a -print_format json -show_chapters -show_format -loglevel error
