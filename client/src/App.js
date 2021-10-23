8fc71cf9e65e64fdbbff509d3c64db7f3237c2f9
Jacob Pfeifer <jacob@pfeifer.dev>
2021-10-23 02:28:36 -0400

    Start to add in ui library components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Navbar, Nav, Button, CustomProvider } from 'rsuite';
+import 'rsuite/styles/index.less';
 
 import {
   BrowserRouter as Router,
@@ -10,25 +12,31 @@ import {
 import Book from './Book.js';
 import Library from './Library.js';
 
+const NavBrand = React.forwardRef(({ href, children, ...rest }, ref) => (
+  <Link ref={ref} to={href} {...rest}>
+    {children}
+  </Link>
+));
+
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
+      <CustomProvider theme="light">
+        <div>
+          <Navbar style={{marginBottom:'8px'}}>
+            <Navbar.Brand as={NavBrand} href="/">
+                Audiobook Server
+            </Navbar.Brand>
+          </Navbar>
 
-        <Switch>
-          <Route path="/book/:book" component={Book} />
-          <Route path="/">
-            <Library />
-          </Route>
-        </Switch>
-      </div>
+          <Switch>
+            <Route path="/book/:book" component={Book} />
+            <Route path="/">
+              <Library />
+            </Route>
+          </Switch>
+        </div>
+      </CustomProvider>
     </Router>
   );
 }
--- client/src/Library.js
+++ client/src/Library.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import { Panel, Row, Col, Nav, Grid } from 'rsuite';
+import './Library.less';
+
+const NavLink = React.forwardRef(({ href, children, ...rest }, ref) => (
+  <Link ref={ref} to={href} {...rest}>
+    {children}
+  </Link>
+));
 
 export default function Library() {
   const [books, setBooks] = useState([]);
@@ -12,10 +20,21 @@ export default function Library() {
   }, []);
 
   return (
-    <div className="Library">
-      {
-        books.map(book => (<Link to={"/book/" + book.path}><p>{book.title}</p></Link>))
-      }
-    </div>
+    <Grid fluid>
+      <Row>
+        {
+
+          books.map(book => (
+            <Link to={"/book/" + book.path}>
+            <Col lg="6" md="12" sm="12" xs="24">
+                <Panel header={book.title} shaded bordered bodyFill style={{display:'flex',flexDirection:'column',height:'360px'}}>
+                      <img style={{minHeight:'0px',minWidth:'0px',maxWidth:'100%',maxHeight:'100%',display:'block',marginLeft:'auto',marginRight:'auto',flexShrink:1}} alt="Book cover art" src={"http://localhost:3001/book/cover?filename="+book.path} />
+                </Panel>
+            </Col>
+            </Link>
+          ))
+        }
+      </Row>
+    </Grid>
   );
 }
